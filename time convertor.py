648f1fc2cd0ed3c7d20f321ceb468f24a5bb705a
Rupesh-1901 <64422429+Rupesh-1901@users.noreply.github.com>
2020-06-26 19:08:08 +0530

    Add files via upload

---
+++ time convertor.py
@@ -0,0 +1,23 @@
+def timeconvertor(str1): 
+	
+	
+	if str1[-2:] == "AM" and str1[:2] == "12": 
+		return "00" + str1[2:-2] 
+		
+	 
+	elif str1[-2:] == "AM": 
+		return str1[:-2] 
+	
+	 
+	elif str1[-2:] == "PM" and str1[:2] == "12": 
+		return str1[:-2] 
+		
+	else: 
+		
+		 
+		return str(int(str1[:2]) + 12) + str1[2:8] 
+
+
+n=str(input("Enter the time in 12hr format: "))
+print(timeconvertor(n))
+
