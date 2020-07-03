757eb75c3e763394b4a78cda79a202f17d95aedf
Rupesh-1901 <64422429+Rupesh-1901@users.noreply.github.com>
2020-07-03 20:50:00 +0530

    Add files via upload

---
+++ day 18(steps to divide).py
@@ -0,0 +1,27 @@
+a=int(input("enter the divident: "))
+b=int(input("enter the divisor: "))
+steps=0
+if(a%b==0):
+    steps=0
+    print(steps)
+else:
+      q, mod = divmod(a, b)
+      a1=(q+1)*b
+      #print(a1)
+      mod1=a1-a
+      #print(mod1)
+      if(mod1>mod):
+               for i in range(0, mod):
+                             steps=steps+1
+                            
+                             print("the least amount of steps needed is :")
+                             print(steps)
+
+      elif(mod>mod1):
+               for i in range(0, mod1):
+                             steps=steps+1
+    
+                             print("the least amount of steps needed is: ")
+                             print(steps)
+          
+    
